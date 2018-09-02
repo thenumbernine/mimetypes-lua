2ec6d7694bad6c13bfeaea938f5f9c74bfe2cd90
Christopher Moore <christopher.e.moore@gmail.com>
2018-09-02 22:48:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+MIME types
+
+This is a simple class that feteches and caches the MIME types associated with file formats.
+
+It gets them from iana.org.
+
+It also automatically associates the .js file format with the .javascript file format.
+
+Depends on my lua-ext and lua-csv projects.
---
+++ mime.lua
@@ -0,0 +1,28 @@
+local class = require 'ext.class'
+local fromlua = require 'ext.fromlua'
+local file = require 'ext.file'
+local MIME = class()
+
+MIME.filename = 'mimetypes.conf'
+
+function MIME:init(filename)
+	 self.filename = filename
+	 self.types = fromlua(file[self.filename] or '')
+	if not self.types then
+		local CSV = require 'csv'
+		self.types = {}
+		for _,source in pairs{'application','audio','image','message','model','multipart','text','video'} do
+			print('fetching '..source..' mime types...')
+			local csv = CSV.string(assert(http.request('http://www.iana.org/assignments/media-types/'..source..'.csv')))
+			csv:setColumnNames(csv.rows:remove(1))
+			for _,row in ipairs(csv.rows) do
+				self.types[row.Name:lower()] = row.Template
+			end
+		end
+		-- well this is strange
+		self.types.js = self.types.js or self.types.javascript
+		file[self.filename] =  tolua(self.types,{indent = true})
+	end
+end
+
+return MIME
